ce93d999d62c1c97461ae6d32b5a01fee1cdc9fe
mykola-mmm <mykola.mazniuk@gmail.com>
2024-02-22 21:15:12 +0100

    terraform fmt

--- day2/output.tf
+++ day2/output.tf
@@ -1,4 +1,4 @@
 output "website_url" {
   description = "Url of the website hosted in the bucket"
-  value = aws_s3_bucket_website_configuration.host_bucket_website_configuration.website_endpoint
+  value       = aws_s3_bucket_website_configuration.host_bucket_website_configuration.website_endpoint
 }
