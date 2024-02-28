ae537471dcb7c1a807df68e2eb24c06564cd61e3
mykola-mmm <mykola.mazniuk@gmail.com>
2024-02-28 20:46:36 +0100

    changed s3 policy to allow access only to cloudfront

--- day2/README.md
+++ day2/README.md
@@ -1,1 +1,4 @@
 Static website on AWS S3 bucket
+
+The access to the s3 bucket is granted only to CloudFront distribution
+
--- day2/main.tf
+++ day2/main.tf
@@ -51,15 +51,15 @@ data "aws_iam_policy_document" "host_bucket_allow_public_access" {
   statement {
     principals {
       type        = "AWS"
-      identifiers = ["*"]
+      # identifiers = ["*"]
+      identifiers = [aws_cloudfront_origin_access_identity.example.iam_arn]
     }
     actions = [
       "s3:GetObject"
     ]
 
     resources = [
-      aws_s3_bucket.host_bucket.arn,
-      "${aws_s3_bucket.host_bucket.arn}/*",
+      "${aws_s3_bucket.host_bucket.arn}/*"
     ]
   }
 }
@@ -136,99 +136,12 @@ resource "aws_s3_object" "host_bucket_files" {
   etag = each.value.digests.md5
 }
 
-# resource "aws_cloudfront_distribution" "cloudfront_distribution" {
-#   aliases                        = []
-#   arn                            = "arn:aws:cloudfront::321206642389:distribution/E1G7N254BFG1J0"
-#   caller_reference               = "4fab7061-b809-40bb-bb89-15a4a25a2bd6"
-#   domain_name                    = "d2mi7wtj9y8c22.cloudfront.net"
-#   enabled                        = true
-#   etag                           = "E189VKW7BWZXWD"
-#   hosted_zone_id                 = "Z2FDTNDATAQYW2"
-#   http_version                   = "http2"
-#   id                             = "E1G7N254BFG1J0"
-#   in_progress_validation_batches = 0
-#   is_ipv6_enabled                = true
-#   last_modified_time             = "2024-02-27 20:19:01.8 +0000 UTC"
-#   price_class                    = "PriceClass_All"
-#   retain_on_delete               = false
-#   status                         = "Deployed"
-#   tags                           = {}
-#   tags_all                       = {}
-#   trusted_key_groups = [
-#     {
-#       enabled = false
-#       items   = []
-#     },
-#   ]
-#   trusted_signers = [
-#     {
-#       enabled = false
-#       items   = []
-#     },
-#   ]
-#   wait_for_deployment = true
-
-#   default_cache_behavior {
-#     allowed_methods = [
-#       "GET",
-#       "HEAD",
-#     ]
-#     cache_policy_id = "658327ea-f89d-4fab-a63d-7e88639e58f6"
-#     cached_methods = [
-#       "GET",
-#       "HEAD",
-#     ]
-#     compress               = true
-#     default_ttl            = 0
-#     max_ttl                = 0
-#     min_ttl                = 0
-#     smooth_streaming       = false
-#     target_origin_id       = "mykola-mmm-s3-bucket-frankly-wrongly-enormous-buzzard.s3-website.eu-central-1.amazonaws.com"
-#     trusted_key_groups     = []
-#     trusted_signers        = []
-#     viewer_protocol_policy = "redirect-to-https"
-#   }
-
-#   origin {
-#     connection_attempts = 3
-#     connection_timeout  = 10
-#     domain_name         = "mykola-mmm-s3-bucket-frankly-wrongly-enormous-buzzard.s3-website.eu-central-1.amazonaws.com"
-#     origin_id           = "mykola-mmm-s3-bucket-frankly-wrongly-enormous-buzzard.s3-website.eu-central-1.amazonaws.com"
-
-#     custom_origin_config {
-#       http_port                = 80
-#       https_port               = 443
-#       origin_keepalive_timeout = 5
-#       origin_protocol_policy   = "http-only"
-#       origin_read_timeout      = 30
-#       origin_ssl_protocols = [
-#         "SSLv3",
-#         "TLSv1",
-#         "TLSv1.1",
-#         "TLSv1.2",
-#       ]
-#     }
-#   }
-
-#   restrictions {
-#     geo_restriction {
-#       locations        = []
-#       restriction_type = "none"
-#     }
-#   }
-
-#   viewer_certificate {
-#     cloudfront_default_certificate = true
-#     minimum_protocol_version       = "TLSv1"
-#   }
+# resource "aws_cloudfront_origin_access_control" "s3_origin_access_control" {
+#   name                              = "example"
+#   origin_access_control_origin_type = "s3"
+#   signing_behavior                  = "always"
+#   signing_protocol                  = "sigv4"
 # }
-
-resource "aws_cloudfront_origin_access_control" "s3_origin_access_control" {
-  name                              = "example"
-  origin_access_control_origin_type = "s3"
-  signing_behavior                  = "always"
-  signing_protocol                  = "sigv4"
-}
 
 locals {
   s3_origin_id = "myS3Origin"
@@ -237,8 +150,12 @@ locals {
 resource "aws_cloudfront_distribution" "s3_distribution" {
   origin {
     domain_name              = aws_s3_bucket.host_bucket.bucket_regional_domain_name
-    origin_access_control_id = aws_cloudfront_origin_access_control.s3_origin_access_control.id
+    # origin_access_control_id = aws_cloudfront_origin_access_control.s3_origin_access_control.id
     origin_id                = local.s3_origin_id
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.example.cloudfront_access_identity_path
+    }
   }
 
   enabled             = true
@@ -277,4 +194,8 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   viewer_certificate {
     cloudfront_default_certificate = true
   }
+}
+
+resource "aws_cloudfront_origin_access_identity" "example" {
+  comment = "Allow cloudfront to access s3 bucket="
 }
